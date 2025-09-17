2e6d55ea74ca3e5fce4458a7fb86267ab1b66cc0
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-09-17 23:32:37 +0300

    feat: counter

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
-<script setup>
+<script>
+  export default {
+    data() {
+      return {
+        likes: 0,
+        dislikes: 0,
+      }
+    },
 
+    methods: {
+      addLike() {
+        this.likes += 1;
+      },
+      addDislike() {
+        this.dislikes += 1;
+      },
+    }
+  }
 </script>
 
 <template>
-  <div>Hello</div>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Likes: <strong>{{ likes }}</strong></div>
+    <div>Dislikes: <strong>{{ dislikes }}</strong></div>
+  </div>
 </template>
 
 <style scoped>
