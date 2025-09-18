a32d36323686c8110393cd51c301b0206ae41dd2
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-09-18 17:17:25 +0300

    feat: animations

--- src/components/PostList/PostList.vue
+++ src/components/PostList/PostList.vue
@@ -17,13 +17,14 @@ export default {
 <template>
   <div v-if="posts.length > 0">
     <h3>Список користувачів</h3>
-    <post-item
-        :post="post"
-        v-for="post in posts"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
-
+    <transition-group name="post-list">
+      <post-item
+          :post="post"
+          v-for="post in posts"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">
     Список постів порожній
@@ -31,5 +32,22 @@ export default {
 </template>
 
 <style scoped>
+  .post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 0.5s ease;
+  }
 
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+
+  .post-list-move {
+    transition: transform 0.5s ease;
+  }
 </style>
